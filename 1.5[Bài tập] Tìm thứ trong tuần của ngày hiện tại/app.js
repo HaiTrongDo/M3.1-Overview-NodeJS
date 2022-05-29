39d827bc8f1f903693496f0d20663330fd1fa5ba
HaiTrongDo <dotronghai91@gmail.com>
2022-05-29 14:16:34 +0700

    update

---
+++ 1.5[Bài tập] Tìm thứ trong tuần của ngày hiện tại/app.js
@@ -0,0 +1,12 @@
+let date = ["chủ nhật", "thứ hai", "thứ ba", "thứ tư", "thứ năm", "thứ sáu", "thứ bảy"]
+
+const event1 = new Date('2020/12/19');console.log(date[event1.getDay()]);
+const event2 = new Date('2020/12/20');console.log(date[event2.getDay()]);
+const event3 = new Date('2020/12/21');console.log(date[event3.getDay()]);
+const event4 = new Date('2020/12/22');console.log(date[event4.getDay()]);
+const event5 = new Date('2020/12/23');console.log(date[event5.getDay()]);
+const event6 = new Date('2020/12/24');console.log(date[event6.getDay()]);
+const event7 = new Date('2020/12/25');console.log(date[event7.getDay()]);
+
+
+
